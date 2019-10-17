deb57a0d4ca12a7dcf4076725584ee4411b7f0b9
IngaZiupsniene <32264243+IngaZiupsniene@users.noreply.github.com>
2019-10-17 12:57:12 +0300

    create project

---
+++ GDPRApp/GDPRApp/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GDPRApp.Model;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GDPRApp.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+     
+        [HttpGet]
+        [EnableCors("CorsPolicy")]
+        public void Get() {}
+
+        [HttpPost]
+        [EnableCors("CorsPolicy")]        
+        public string Post([FromBody] AppUser appuser)
+        {
+           return appuser.UserEmail;
+        }
+
+       
+    }
+}
